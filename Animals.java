9aaecfaf2674ea3a87bc68b1ef772d88d4eacf63
nur-21-89 <tiryakinur8@gmail.com>
2025-07-27 18:36:11 +0300

    Animals

---
+++ Animals.java
@@ -0,0 +1,26 @@
+package UcuncuHafta;
+
+public class Animals {
+    String name;
+    String color;
+    String cins;
+
+    Animals (String name, String color ,String cins){
+    this.cins = cins;
+    this.name = name;
+    this.color = color;
+}
+
+String sounds(){
+        return sounds ();
+
+}
+
+void printInfo(){
+    System.out.println("============");
+    System.out.println("Hayvanın İsmi:\t"+this.name);
+    System.out.println("Hayvanın Rengi:\t"+this.color);
+    System.out.println("Hayvanın Cinsi:\t"+this.cins);
+
+}
+}
---
+++ AnimalsTest.java
@@ -0,0 +1,11 @@
+package UcuncuHafta;
+
+public class AnimalsTest {
+    public static void main (String[] args){
+        Animals cat = new Animals("mecnun","gri", "british");
+        cat.printInfo();
+
+        Animals dog = new Animals("Mom","Turuncu", "Poodle");
+        dog.printInfo();
+    }
+}
